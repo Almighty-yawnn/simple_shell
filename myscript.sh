63c15fa14675507b5f6f4dec23c63c7ef2e25450
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-07-26 14:56:50 -0700

    hs

---
+++ myscript.sh
@@ -0,0 +1,23 @@
+echo "Hello, World"
+ls
+pwd
+date
+cal
+who
+ps
+df
+du
+uname -a
+echo "Testing 1..2..3.."
+cat /etc/passwd
+grep "command" path.c
+mkdir test_directory
+rm -r test_directory
+touch myfile.txt
+chmod +x myscript.sh
+ls -l 
+head -n 5 path.c
+tail -n 5 builtins.c
+wc myfile.txt
+sort AUTHORS
+echo $HOME
